1d37d63bbb93437b3c44a8df16e1393fb5890d8f
Lucas <lluc23@hotmail.com>
2025-03-28 20:38:43 +0100

    clean and fix logs
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,20 @@ use esp_hal::{
 // You need a panic handler. Usually, you you would use esp_backtrace, panic-probe, or
 // something similar, but you can also bring your own like this:
 #[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    log::info!("error {}", info);
     esp_hal::system::software_reset()
 }
 
 #[main]
 fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
     log::info!("Hello world!");
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     log::info!("Hello world!");
     let peripherals = esp_hal::init(config);
     log::info!("Hello world!");
-    esp_println::logger::init_logger_from_env();
-    log::info!("Hello world!");
+
     // Set GPIO0 as an output, and set its state high initially.
     let mut led = Output::new(peripherals.GPIO0, Level::High, OutputConfig::default());
     log::info!("Hello world!");
