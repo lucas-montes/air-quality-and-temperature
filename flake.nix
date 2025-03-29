c3627ba410ee184de615f38877dd2674c94b117d
Lucas <lluc23@hotmail.com>
2025-03-29 19:03:59 +0100

    make it work
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
           mkdir -p "$RUSTUP_HOME" "$CARGO_HOME"
 
           # Update PATH to include Cargo binaries
-          #          export PATH="$CARGO_HOME/bin:$PATH"
+          export PATH="$CARGO_HOME/bin:$PATH"
 
           # Install the Xtensa toolchain via espup if not already present
           if [ ! -f rust-toolchain.env ]; then
@@ -47,12 +47,47 @@
 
           # Source the toolchain environment
           source rust-toolchain.env
+          rustup default esp
 
           echo "Xtensa ESP32 development environment ready!"
           echo "Rust version: $(rustc --version)"
           echo "Use 'cargo build --target xtensa-esp32-none-elf' to build."
           echo "Use 'cargo espflash flash' to flash your binary."
         '';
+      };
+
+      packages.default = pkgs.rustPlatform.buildRustPackage {
+        pname = "xtensa-example";
+        version = "0.1.0";
+        src = pkgs.lib.cleanSource ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock; # Assumes you have a Cargo.lock
+        };
+        nativeBuildInputs = with pkgs; [cargo-espflash rustup];
+        buildPhase = ''
+          export RUSTUP_HOME="$PWD/.rustup"
+          export CARGO_HOME="$PWD/.cargo"
+          mkdir -p "$RUSTUP_HOME" "$CARGO_HOME"
+          ${pkgs.espup}/bin/espup install --export-file rust-toolchain.env
+          source rust-toolchain.env
+          cargo build --release --target xtensa-esp32-none-elf
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp target/xtensa-esp32-none-elf/release/xtensa-example $out/bin/
+        '';
+      };
+
+      apps.default = {
+        type = "app";
+        program = "${pkgs.writeShellScriptBin "flash" ''
+          export RUSTUP_HOME="$PWD/.rustup"
+          export CARGO_HOME="$PWD/.cargo"
+          mkdir -p "$RUSTUP_HOME" "$CARGO_HOME"
+          ${pkgs.espup}/bin/espup install --export-file rust-toolchain.env
+          source rust-toolchain.env
+          ${pkgs.cargo-espflash}/bin/cargo-espflash flash --release --target xtensa-esp32-none-elf
+        ''}/bin/flash";
       };
     });
 }
