9cc45981fc5413002e781120f6e600137ea8cf4a
Lucas <lluc23@hotmail.com>
2025-04-13 12:39:16 +0200

    bluetooth stopped working

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod ens160_plus_aht21;
 mod mq135;
 mod utils;
 
+use core::{fmt::Write, ptr::addr_of_mut};
+
 use bleps::{
     ad_structure::{
         create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
@@ -16,9 +18,12 @@ use bleps::{
     attribute_server::NotificationData,
     gatt,
 };
-use core::{fmt::Write, ptr::addr_of_mut};
+
 use embassy_executor::Spawner;
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
+use embassy_sync::{
+    blocking_mutex::raw::CriticalSectionRawMutex, channel::Channel, signal::Signal,
+};
+use embassy_time::Timer;
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
@@ -26,14 +31,13 @@ use embedded_graphics::{
     text::{Baseline, Text},
     Drawable,
 };
-use embedded_hal::delay::DelayNs;
 use embedded_hal_bus::{i2c::AtomicDevice, util::AtomicCell};
 use ens160::{AirQualityIndex, Ens160};
 use esp_hal::{
     analog::adc::{Adc, AdcConfig, Attenuation},
     clock::CpuClock,
     delay::Delay,
-    gpio::{Flex, Level, Output, OutputConfig, Pull},
+    gpio::{Flex, Output, Pull},
     i2c::master::{BusTimeout, Config, I2c},
     peripherals::ADC1,
     rng::Rng,
@@ -42,7 +46,7 @@ use esp_hal::{
     timer::{timg::TimerGroup, AnyTimer},
 };
 use esp_hal_embassy::Executor;
-use esp_println::logger;
+
 use static_cell::StaticCell;
 
 use dht11::Dht11;
@@ -55,8 +59,6 @@ use ssd1306::{
     I2CDisplayInterface, Ssd1306,
 };
 use utils::{RollingMedian, StrWriter};
-
-use embassy_time::Timer;
 
 // When you are okay with using a nightly compiler it's better to use https://docs.rs/static_cell/2.1.0/static_cell/macro.make_static.html
 macro_rules! mk_static {
@@ -95,8 +97,28 @@ async fn control_led(
     }
 }
 
+struct SensorsData {
+    temperature: f32,
+    humidity: f32,
+    eco2: u16,
+    tvoc: u16,
+}
+
+impl SensorsData {
+    fn to_bytes(&self) -> [u8; 12] {
+        let mut bytes = [0u8; 12];
+        bytes[0..4].copy_from_slice(&self.temperature.to_le_bytes());
+        bytes[4..8].copy_from_slice(&self.humidity.to_le_bytes());
+        bytes[8..10].copy_from_slice(&self.eco2.to_le_bytes());
+        bytes[10..12].copy_from_slice(&self.tvoc.to_le_bytes());
+        bytes
+    }
+}
+
+//static SHARED: Channel<CriticalSectionRawMutex, SensorsData, 30> = Channel::new();
+
 #[esp_hal_embassy::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(_spawner: Spawner)  {
     esp_println::logger::init_logger_from_env();
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
@@ -113,89 +135,116 @@ async fn main(_spawner: Spawner) -> ! {
     let timg0 = TimerGroup::new(peripherals.TIMG1);
     let timer0: AnyTimer = timg0.timer0.into();
     let timer1: AnyTimer = timg0.timer1.into();
-    esp_hal_embassy::init([timer0, timer1]);
+      esp_hal_embassy::init([timer0, timer1]);
+
+
+    // let channel = Channel::<CriticalSectionRawMutex, SensorsData, 30>::new();
+
+    // let tx = channel.sender();
+    // let rx = channel.receiver();
 
     let mut cpu_control = CpuControl::new(peripherals.CPU_CTRL);
+       let _guard =
+           match cpu_control.start_app_core(unsafe { &mut *addr_of_mut!(APP_CORE_STACK) }, move || {
+               static EXECUTOR: StaticCell<Executor> = StaticCell::new();
+               let executor = EXECUTOR.init(Executor::new());
+               executor.run(|spawner| {
+                   if let Err(err) = spawner.spawn(data_loop(
+                       peripherals.GPIO5,
+                       peripherals.GPIO21,
+                       peripherals.GPIO22,
+                       peripherals.GPIO34,
+                       peripherals.ADC1,
+                       peripherals.I2C0,
+                       //tx
+                   )) {
+                       log::error!("spawnner {}", err);
+                   }
+               });
+           }) {
+               Ok(g) => g,
+               Err(err) => {
+                   log::error!("Error starting app core: {:?}", err);
+                   loop {}
+               }
+           };
 
-    static LED_CTRL: StaticCell<Signal<CriticalSectionRawMutex, bool>> = StaticCell::new();
-    let led_ctrl_signal = &*LED_CTRL.init(Signal::new());
+    bluetooth_loop(esp_wifi_ctrl, peripherals.BT).await
+}
 
+async fn bluetooth_loop(
+    esp_wifi_ctrl: &'static EspWifiController<'static>,
+    mut bluetooth: esp_hal::peripherals::BT,
+) {
+    log::info!("bluetooth started {}", Cpu::current() as usize);
+    let connector = BleConnector::new(esp_wifi_ctrl, &mut bluetooth);
 
-    log::info!("all started well");
-
-    let _guard =
-        match cpu_control.start_app_core(unsafe { &mut *addr_of_mut!(APP_CORE_STACK) }, move || {
-            static EXECUTOR: StaticCell<Executor> = StaticCell::new();
-            let executor = EXECUTOR.init(Executor::new());
-            executor.run(|spawner| {
-                if let Err(err) = spawner.spawn(data_loop(
-                    peripherals.GPIO5,
-                    peripherals.GPIO21,
-                    peripherals.GPIO22,
-                    peripherals.GPIO34,
-                    peripherals.ADC1,
-                    peripherals.I2C0,
-                )) {
-                    log::error!("spawnner {}", err);
-                }
-            });
-        }) {
-            Ok(g) => g,
+    let now = || time::Instant::now().duration_since_epoch().as_millis();
+    let mut ble = Ble::new(connector, now);
+    loop {
+        match ble.init().await {
+            Ok(e) => {
+                log::info!("Bluetooth initialized {:?}", e);
+            }
             Err(err) => {
-                log::error!("Error starting app core: {:?}", err);
+                log::error!("Error initializing bluetooth: {:?}", err);
+                loop {}
+            }
+        }
+        match ble.cmd_set_le_advertising_parameters().await {
+            Ok(e) => {
+                log::info!("cmd_set_le_advertising_parameters {:?}", e);
+            }
+            Err(err) => {
+                log::error!("Error cmd_set_le_advertising_parameters: {:?}", err);
+                loop {}
+            }
+        }
+
+        let data = match create_advertising_data(&[
+            AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+            AdStructure::CompleteLocalName("RoomTempIq"),
+            AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
+        ]) {
+            Ok(data) => data,
+            Err(err) => {
+                log::error!("Error creating advertising data: {:?}", err);
                 loop {}
             }
         };
 
-    let mut bluetooth = peripherals.BT;
+        match ble.cmd_set_le_advertising_data(data).await {
+            Ok(e) => {
+                log::info!("cmd_set_le_advertising_data {:?}", e);
+            }
+            Err(err) => {
+                log::error!("Error cmd_set_le_advertising_data: {:?}", err);
+                loop {}
+            }
+        }
 
-    let connector = BleConnector::new(&esp_wifi_ctrl, &mut bluetooth);
-
-    let now = || time::Instant::now().duration_since_epoch().as_millis();
-    let mut ble = Ble::new(connector, now);
-    log::info!("Connector created");
-
-    loop {
-        log::info!("{:?}", ble.init().await);
-        log::info!("{:?}", ble.cmd_set_le_advertising_parameters().await);
-        log::info!(
-            "{:?}",
-            ble.cmd_set_le_advertising_data(
-                create_advertising_data(&[
-                    AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
-                    AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
-                    AdStructure::CompleteLocalName(esp_hal::chip!()),
-                ])
-                .unwrap()
-            )
-            .await
-        );
-        log::info!("{:?}", ble.cmd_set_le_advertise_enable(true).await);
+        match ble.cmd_set_le_advertise_enable(true).await {
+            Ok(e) => {
+                log::info!("cmd_set_le_advertise_enable {:?}", e);
+            }
+            Err(err) => {
+                log::error!("Error cmd_set_le_advertise_enable: {:?}", err);
+                loop {}
+            }
+        }
 
         log::info!("started advertising");
 
-        let mut rf = |_offset: usize, data: &mut [u8]| {
-            data[..20].copy_from_slice(&b"Hello Bare-Metal BLE"[..]);
-            17
-        };
-        let mut wf = |offset: usize, data: &[u8]| {
-            log::info!("RECEIVED: {} {:?}", offset, data);
-            log::info!("Sending LED on");
-            led_ctrl_signal.signal(true);
-        };
-
-        let mut wf2 = |offset: usize, data: &[u8]| {
-            log::info!("RECEIVED: {} {:?}", offset, data);
-            log::info!("Sending LED off");
-            led_ctrl_signal.signal(false);
-        };
-
         let mut rf3 = |_offset: usize, data: &mut [u8]| {
             data[..5].copy_from_slice(&b"Hola!"[..]);
+            log::info!("Sending LED on");
             5
         };
         let mut wf3 = |offset: usize, data: &[u8]| {
-            log::info!("RECEIVED: Offset {}, data {:?}", offset, data);
+            log::info!("Sending LED off");
+        };
+        let mut wf2 = |offset: usize, data: &[u8]| {
+            log::info!("RECEIVED: {} {:?}", offset, data);
         };
 
         gatt!([service {
@@ -208,51 +257,44 @@ async fn main(_spawner: Spawner) -> ! {
                     read: rf3,
                     write: wf3,
                 },
+                characteristic {
+                    uuid: "957312e0-2354-11eb-9f10-fbc30a62cf38",
+                    write: wf2,
+                },
             ],
         },]);
 
         let mut rng = bleps::no_rng::NoRng;
         let mut srv = AttributeServer::new(&mut ble, &mut gatt_attributes, &mut rng);
 
-        let counter = core::cell::RefCell::new(0u8);
-        let counter = &counter;
-
         let mut notifier = || {
-            // TODO how to check if notifications are enabled for the characteristic?
-            // maybe pass something into the closure which just can query the characteristic
-            // value probably passing in the attribute server won't work?
-
             async {
-                let mut val;
-
-                loop {
-                    val = scale_random_u32(rando.clone().random());
-                    if val >= 100 {
-                        break;
-                    }
-                    Timer::after_millis(500).await;
-                }
-                let mut data = [0u8; 13];
-                data.copy_from_slice(b"Notification0");
-                {
-                    let mut counter = counter.borrow_mut();
-                    data[data.len() - 1] += *counter;
-                    *counter = (*counter + 1) % 10;
-                }
-                NotificationData::new(my_characteristic_handle, &data)
+                // let data = SHARED.receive().await;
+                let data = SensorsData {
+                    temperature: 33.3,
+                    humidity: 33.3,
+                    eco2: 33,
+                    tvoc: 33,
+                };
+                NotificationData::new(my_characteristic_handle, &data.to_bytes())
             }
         };
 
-        srv.run(&mut notifier).await.unwrap();
+        log::info!("starting srving");
+        match embassy_time::with_timeout(
+            embassy_time::Duration::from_secs(1),
+            srv.run(&mut notifier),
+        )
+        .await
+        {
+            Ok(Ok(())) => log::info!("srv.run is ok"),
+            Ok(Err(err)) => log::error!("error in srv.run {:?}", err),
+            Err(_) => log::debug!("srv.run timed out, retrying"),
+        };
+
+        log::info!("srv worked");
     }
 }
-
-const fn scale_random_u32(rand_val: u32) -> u32 {
-    let min = 60;
-    let max = 110;
-    min + (rand_val % (max - min + 1))
-}
-
 
 #[embassy_executor::task]
 async fn data_loop(
@@ -262,9 +304,10 @@ async fn data_loop(
     adc_pin: esp_hal::gpio::GpioPin<34>,
     adc1: ADC1,
     i2c0: esp_hal::peripherals::I2C0,
+    // tx: embassy_sync::channel::Sender<'static, CriticalSectionRawMutex, SensorsData, 30>
 ) {
     log::info!("data started {}", Cpu::current() as usize);
-    let mut delay = Delay::new();
+    let delay = Delay::new();
     let mut capteur = Flex::new(dht11_pin);
     capteur.set_as_open_drain(Pull::None);
     let mut dht11 = Dht11::new(capteur, delay);
@@ -302,7 +345,7 @@ async fn data_loop(
         if ready && ready1 {
             break;
         }
-        delay.delay_ms(3000);
+        Timer::after_millis(500).await;
         if !ready {
             if let Err(err) = aht.init() {
                 log::error!("AHT error: {:?}", err);
@@ -312,13 +355,13 @@ async fn data_loop(
         }
 
         if !ready1 {
-            delay.delay_ms(500);
+            Timer::after_millis(500).await;
             if let Err(err) = ens160.operational() {
                 log::error!("ens160 error: {:?}", err);
             } else {
                 ready1 = true;
             }
-            delay.delay_ms(500);
+            Timer::after_millis(500).await;
         }
     }
 
@@ -331,17 +374,12 @@ async fn data_loop(
     let mut eco2_average = RollingMedian::<256, u16>::new("eCO2");
     let mut tvoc_average = RollingMedian::<256, u16>::new("TVOC ppb");
     let mut c02_average = RollingMedian::<256, f32>::new("CO2 ppm");
-    let mut show_heap = 0;
+
     loop {
-        show_heap += 1;
-        delay.delay_ms(500);
+        Timer::after_millis(500).await;
 
         display.clear_buffer();
 
-        if show_heap % 3 == 0 {
-            log::info!("memory core 1");
-            log::info!("{}", esp_alloc::HEAP.stats());
-        };
         // Get air quality from Ens160
         let quality_str = match ens160.air_quality_index().unwrap() {
             AirQualityIndex::Excellent => "Air Quality: Excellent",
@@ -353,11 +391,12 @@ async fn data_loop(
         Text::with_baseline(quality_str, Point::new(0, 0), text_style, Baseline::Top)
             .draw(&mut display)
             .unwrap();
-
+        Timer::after_nanos(1).await;
         if let Ok(co2_ppm) = senseur.get_ppm() {
             c02_average.update(co2_ppm);
             //log::info!("CO2 PPM: {}", co2_ppm);
         };
+        Timer::after_nanos(1).await;
         let mut writer = StrWriter {
             buffer: &mut [0u8; 32],
             pos: 0,
@@ -371,7 +410,7 @@ async fn data_loop(
         )
         .draw(&mut display)
         .unwrap();
-
+        Timer::after_nanos(1).await;
         // Get Eco2 from Ens160
         if let Ok(eco2) = ens160.eco2() {
             eco2_average.update(*eco2);
@@ -382,6 +421,7 @@ async fn data_loop(
             buffer: &mut [0u8; 32],
             pos: 0,
         };
+        Timer::after_nanos(1).await;
         let _ = write!(writer, "{}", &eco2_average);
         Text::with_baseline(
             writer.as_str(),
@@ -391,12 +431,13 @@ async fn data_loop(
         )
         .draw(&mut display)
         .unwrap();
-
+        Timer::after_nanos(1).await;
         // Get TVOC from Ens160
         if let Ok(tvoc) = ens160.tvoc() {
             tvoc_average.update(tvoc);
             // log::info!("TVOC: {}", tvoc);
         }
+        Timer::after_nanos(1).await;
         let mut writer = StrWriter {
             buffer: &mut [0u8; 32],
             pos: 0,
@@ -410,16 +451,16 @@ async fn data_loop(
         )
         .draw(&mut display)
         .unwrap();
-
+        Timer::after_nanos(1).await;
         // Read ENS160
         if let Ok((raw_temp, raw_hum)) = ens160.temp_and_hum() {
             let temp = raw_temp as f32 / 100.0;
             let hum = raw_hum as f32 / 100.0;
-            log::info!("ENS160 temp: {} C, hum: {} %", temp, hum);
+            // log::info!("ENS160 temp: {} C, hum: {} %", temp, hum);
             temperature_average.update(temp);
             humidity_average.update(hum);
         }
-
+        Timer::after_nanos(1).await;
         // Read DHT11
         if let Ok(reading) = dht11.read() {
             // log::info!(
@@ -430,13 +471,14 @@ async fn data_loop(
             temperature_average.update(reading.temperature() as f32);
             humidity_average.update(reading.humidity() as f32);
         }
-
+        Timer::after_nanos(1).await;
         // Read AHT21
         if let Ok((hum, temp)) = aht.read() {
             // log::info!("AHT21 temp: {} C, hum: {} %", temp.celsius(), hum.rh());
             temperature_average.update(temp.celsius());
             humidity_average.update(hum.rh());
         }
+        Timer::after_nanos(1).await;
 
         let mut writer = StrWriter {
             buffer: &mut [0u8; 32],
@@ -451,7 +493,7 @@ async fn data_loop(
         )
         .draw(&mut display)
         .unwrap();
-
+        Timer::after_nanos(1).await;
         let mut writer = StrWriter {
             buffer: &mut [0u8; 32],
             pos: 0,
@@ -465,7 +507,19 @@ async fn data_loop(
         )
         .draw(&mut display)
         .unwrap();
-
+        Timer::after_nanos(1).await;
         display.flush().unwrap();
+
+        let data = SensorsData {
+            temperature: temperature_average.median(),
+            humidity: humidity_average.median(),
+            eco2: eco2_average.median(),
+            tvoc: tvoc_average.median(),
+        };
+
+        //         SHARED.send(
+        // data
+
+        //         ).await;
     }
 }
